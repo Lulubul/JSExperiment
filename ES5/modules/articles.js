15c9654a7a6734be04903dbd2c0dfe34d5bdd4b7
Lulubul <robert.unguru@gmail.com>
2018-08-08 21:56:38 +0300

    Add gitignore file and eslint

--- ES5/modules/articles.js
+++ ES5/modules/articles.js
@@ -1,27 +1,27 @@
-var Articles = (function() {
+var Articles = (function () {
 
-    var articles = [
-        "To Reach an Audience of Million, Create for an Audience of One",
-        "One road to being a cartoonist",
-        "The Purpose of Travelling",
-        "Mourning the Writer I Used to Be"
-    ];
+	var articles = [
+		'To Reach an Audience of Million, Create for an Audience of One',
+		'One road to being a cartoonist',
+		'The Purpose of Travelling',
+		'Mourning the Writer I Used to Be'
+	];
 
-    function getArticles() {
-        return articles;
-    }
+	function getArticles() {
+		return articles;
+	}
 
-    function getArticleElements() {
-        return articles.map(function(articleTitle) {
-            var article = document.createElement("article");        // Create a <article> element
-            var text = document.createTextNode(articleTitle);       // Create a text node
-            article.appendChild(text);
-            return article;
-        });
-    }
+	function getArticleElements() {
+		return articles.map(function (articleTitle) {
+			var article = document.createElement('article'); // Create a <article> element
+			var text = document.createTextNode(articleTitle); // Create a text node
+			article.appendChild(text);
+			return article;
+		});
+	}
 
-    return {
-        getArticles: getArticles,
-        getArticleElements: getArticleElements
-    }
+	return {
+		getArticles: getArticles,
+		getArticleElements: getArticleElements
+	};
 })();
--- ES5/modules/header.js
+++ ES5/modules/header.js
@@ -1,33 +1,33 @@
-var Header = (function() {
+var Header = (function () {
 
-    var menuTitles = [
-        "HOME",
-        "GREAT ESCAPE",
-        "CULTURE",
-        "TECH",
-        "ENTREPRENEURSHIP",
-        "SELF",
-        "POLITICS",
-        "DESIGN",
-        "SCIENCE",
-        "POPULAR"
-    ];
+	var menuTitles = [
+		'HOME',
+		'GREAT ESCAPE',
+		'CULTURE',
+		'TECH',
+		'ENTREPRENEURSHIP',
+		'SELF',
+		'POLITICS',
+		'DESIGN',
+		'SCIENCE',
+		'POPULAR'
+	];
 
-    function getMenuTitles() {
-        return menuTitles;
-    }
+	function getMenuTitles() {
+		return menuTitles;
+	}
 
-    function getMenuElements() {
-        return menuTitles.map(function(title) {
-            var li = document.createElement("li");        // Create a <li> element
-            var text = document.createTextNode(title);       // Create a text node
-            li.appendChild(text);
-            return li;
-        });
-    }
+	function getMenuElements() {
+		return menuTitles.map(function (title) {
+			var li = document.createElement('li'); // Create a <li> element
+			var text = document.createTextNode(title); // Create a text node
+			li.appendChild(text);
+			return li;
+		});
+	}
 
-    return {
-        getMenuTitles: getMenuTitles,
-        getMenuElements: getMenuElements
-    }
+	return {
+		getMenuTitles: getMenuTitles,
+		getMenuElements: getMenuElements
+	};
 })();
